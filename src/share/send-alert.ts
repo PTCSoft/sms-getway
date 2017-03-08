4620088bb7505817bca025fea8528b936bd8e8e3
Ali Mihandoost <i@ali.md>
2017-03-08 16:28:22 +0330

    feat(send-alert): send alert to ptc telegram service

--- src/share/send-alert.ts
+++ src/share/send-alert.ts
@@ -1,13 +1,21 @@
 import l10n from '../share/l10n';
 import {alertUrl as alertBaseUrl, alertToken} from './config';
 const alertUrl: string = `${alertBaseUrl}/v2/sendMessage/${alertToken}`;
+import request from './1request';
 
 import * as debug from 'debug';
-const log: debug.IDebugger = debug('send-alert');
+const log: debug.IDebugger = debug('psg:send-alert');
 
 export default async function sendAlert (code: number, extra: any, file?: any) {
+  log(code);
+
+  if (!alertToken) {
+    log('Alert token not found!');
+    return;
+  }
+
   const data: any = {
-    title: 'PTC Telegram Service Error!',
+    title: 'SMS Getway Error!',
     id: `error_${code}`,
     description: l10n(`error_${code}`),
     extra: extra,
@@ -19,15 +27,41 @@ export default async function sendAlert (code: number, extra: any, file?: any) {
     data['file'] = file;
   }
 
-  return await sendTelegramMessage(data);
+  try {
+    const resolute = await sendTelegramMessage(data);
+    log(resolute.body);
+    if (resolute.body && resolute.body.ok) {
+      return resolute.body;
+    }
+    else {
+      log('Error in sending alert!');
+      log(resolute.response);
+    }
+  }
+  catch (err) {
+    log('Error in request');
+    log(err);
+  }
 }
 
 export async function sendTelegramMessage (data: any) {
   log(data);
+
   if (!alertToken) {
-    log('Alert token not found!');
-    return;
+    throw('Alert token not found!');
   }
 
+  const requestOption: any = {
+    method: 'POST',
+    url: alertUrl,
+    headers: {
+      'cache-control': 'no-cache',
+      'content-type': 'application/json',
+    },
+    body: data,
+    gzip: true,
+    json: true,
+  }
 
+  return request(requestOption);
 }
