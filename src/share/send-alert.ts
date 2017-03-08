ea9740705d9c8d1e7966e16d991eee0e46aca4fa
Ali Mihandoost <i@ali.md>
2017-03-08 10:21:22 +0330

    feat(send-alert): base method

--- src/share/send-alert.ts
+++ src/share/send-alert.ts
@@ -1,2 +1,33 @@
+import l10n from '../share/l10n';
 import {alertUrl as alertBaseUrl, alertToken} from './config';
 const alertUrl: string = `${alertBaseUrl}/v2/sendMessage/${alertToken}`;
+
+import * as debug from 'debug';
+const log: debug.IDebugger = debug('send-alert');
+
+export default async function sendAlert (code: number, extra: any, file?: any) {
+  const data: any = {
+    title: 'PTC Telegram Service Error!',
+    id: `error_${code}`,
+    description: l10n(`error_${code}`),
+    extra: extra,
+    sendAllContent: true,
+    // parseMode: 'Markdown'
+  };
+
+  if (file) {
+    data['file'] = file;
+  }
+
+  return await sendTelegramMessage(data);
+}
+
+export async function sendTelegramMessage (data: any) {
+  log(data);
+  if (!alertToken) {
+    log('Alert token not found!');
+    return;
+  }
+
+
+}
