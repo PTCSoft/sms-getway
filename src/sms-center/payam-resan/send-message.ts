3e0c6df70ffbd9b200bd1c3e45fcb5047f54285b
Ali Mihandoost <i@ali.md>
2017-03-09 15:08:27 +0330

    feat(payam-resan-send): sendmessage methods

--- src/sms-center/payam-resan/send-message.ts
+++ src/sms-center/payam-resan/send-message.ts
@@ -1,5 +1,8 @@
+import request from '../../share/1request';
 import * as debug from 'debug';
 const log: debug.IDebugger = debug('psg:sms:payamresan:sendmsg');
+
+export const url = "http://www.payam-resan.com/APISend.aspx";
 
 type sendParameter = {
   to:       string,
@@ -10,5 +13,37 @@ type sendParameter = {
 };
 
 export async function sendMessage (parameter: sendParameter): Promise<any> {
-  return 'eyval! ' + parameter.text;
+  const requestOption: any = {
+    method: 'POST',
+    url: url,
+    headers: {
+      'cache-control': 'no-cache',
+      'content-type': 'multipart/form-data',
+    },
+    formData:  {
+      Username: parameter.username,
+      Password: parameter.password,
+      From: parameter.from,
+      To: parameter.to,
+      Text: parameter.text
+    }
+  }
+
+  log(requestOption)
+
+  const resolute = await request(requestOption);
+
+  const body = resolute.body ? resolute.body.trim() : null;
+
+  if (body === '1' ) {
+    return true;
+  }
+
+  else if (body === '0') {
+    return false;
+  }
+
+  else {
+    throw {code: 107, extra: body};
+  }
 }
